5e51f9edf7e0bf6ad2fa0ced1096427d4e985500
ashokjr99 <ashoksharmajr@gmail.com>
2024-03-11 18:03:46 -0500

    e3

---
+++ react_router/src/components/Welcome.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink, Routes, Route } from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+
+      <NavLink to="new-user">Enter</NavLink>
+
+      <Routes>
+        <Route path="new-user" element={<WelcomeUser />} />
+      </Routes>
+    </div>
+  );
+};
+
+const WelcomeUser = () => {
+  return <h1>Hey User!</h1>;
+};
+
+export default Welcome;
